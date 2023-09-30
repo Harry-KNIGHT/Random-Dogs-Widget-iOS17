7b67242e875d2d8c851c98d3b4778c99d6664efb
Harry-KNIGHT <eliott.knight@icloud.com>
2023-09-30 22:46:11 +0200

    feat: ad widget extension

---
+++ InteractiveDogWidgets/InteractiveDogWidgets.swift
@@ -0,0 +1,86 @@
+//
+//  InteractiveDogWidgets.swift
+//  InteractiveDogWidgets
+//
+//  Created by Elliot Knight on 30/09/2023.
+//
+
+import WidgetKit
+import SwiftUI
+import API
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+		SimpleEntry(date: Date(), emoji: "😀", dog: .init(imageUrl: ""))
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀", dog: .init(imageUrl: ""))
+        completion(entry)
+    }
+
+	@MainActor
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+
+			Task {
+				do {
+					let dog = try await DogServiceDefault().fetchRandomDog()
+					let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+					let entry = SimpleEntry(date: entryDate, emoji: "😀", dog: dog)
+					entries.append(entry)
+					
+					let timeline = Timeline(entries: entries, policy: .atEnd)
+					completion(timeline)
+				} catch {
+					throw error
+				}
+			}
+        }
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+	let dog: DogData
+}
+
+struct InteractiveDogWidgetsEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+			NetworkImage(url: URL(string: entry.dog.imageUrl))
+        }
+    }
+}
+
+struct InteractiveDogWidgets: Widget {
+    let kind: String = "InteractiveDogWidgets"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                InteractiveDogWidgetsEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                InteractiveDogWidgetsEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    InteractiveDogWidgets()
+} timeline: {
+	SimpleEntry(date: .now, emoji: "😀", dog: .init(imageUrl: ""))
+	SimpleEntry(date: .now, emoji: "🤩", dog: .init(imageUrl: ""))
+}
---
+++ InteractiveDogWidgets/InteractiveDogWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  InteractiveDogWidgetsBundle.swift
+//  InteractiveDogWidgets
+//
+//  Created by Elliot Knight on 30/09/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct InteractiveDogWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        InteractiveDogWidgets()
+    }
+}
