e882113108d696bcb61c353371ea9952017ec2f2
Harry-KNIGHT <eliott.knight@icloud.com>
2023-09-30 09:44:06 +0200

    feat: fetch data and display it on view

---
+++ RandomDogs/RandomDogView.swift
@@ -0,0 +1,41 @@
+//
+//  RandomDogView.swift
+//  RandomDogs
+//
+//  Created by Elliot Knight on 30/09/2023.
+//
+
+import SwiftUI
+import API
+
+struct RandomDogView: View {
+	@StateObject private var dogViewModel = RandomDogViewModel(service: DogServiceDefault())
+	
+	var body: some View {
+		VStack {
+			Spacer()
+			AsyncImage(url: URL(string: dogViewModel.dog?.imageUrl ?? "")) { image in
+				image
+					.resizable()
+					.scaledToFit()
+			} placeholder: {
+				ProgressView()
+					.font(.title2)
+					.padding()
+			}
+			Spacer()
+			Button(action: {
+				dogViewModel.callDog()
+			}, label: {
+				Text("Fetch new image")
+					.font(.title2)
+			})
+			.buttonStyle(.bordered)
+			.buttonBorderShape(.roundedRectangle(radius: 10))
+		}
+	}
+}
+
+#Preview {
+	RandomDogView()
+}
---
+++ RandomDogs/RandomDogViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  RandomDogViewModel.swift
+//  RandomDogs
+//
+//  Created by Elliot Knight on 30/09/2023.
+//
+
+import Foundation
+import API
+
+final class RandomDogViewModel: ObservableObject {
+
+	@Published var dog: DogData?
+
+	private let service: DogService
+
+	init(service: DogService) {
+		self.service = service
+
+		callDog()
+	}
+	
+	// MARK: Public methods
+
+	public func callDog() {
+		Task {
+			await getDog()
+		}
+	}
+
+	// MARK: Private methods
+	
+	@MainActor
+	private func getDog() async {
+		do {
+			dog = try await service.fetchRandomDog()
+		} catch {
+			dog = nil
+		}
+	}
+}
