af70a7a6a23898f48b8a419b9c420913b866b4ce
Harry-KNIGHT <eliott.knight@icloud.com>
2023-09-30 09:13:37 +0200

    second commit

---
+++ RandomDogs/RandomDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomDogsApp.swift
+//  RandomDogs
+//
+//  Created by Elliot Knight on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
