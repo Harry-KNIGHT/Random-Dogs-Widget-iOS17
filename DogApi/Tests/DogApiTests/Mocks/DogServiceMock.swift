acd86ab21a5d302a3573817da70cf9d06b1d4a84
Harry-KNIGHT <eliott.knight@icloud.com>
2023-10-01 14:31:48 +0200

    feat: add api lib

---
+++ DogApi/Tests/DogApiTests/Mocks/DogServiceMock.swift
@@ -0,0 +1,8 @@
+//
+//  DogServiceMock.swift
+//  
+//
+//  Created by Elliot Knight on 01/10/2023.
+//
+
+import Foundation
---
+++ DogApi/Tests/DogApiTests/Resources/ValidDogDataMock.json
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Elliot Knight on 01/10/2023.
+//
+
+import Foundation
